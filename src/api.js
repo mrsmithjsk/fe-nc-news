2aa6b717955a1f688349af1141d0ddd0f1d7610b
Jack Smith <mr.smithjsk@gmail.com>
2024-02-06 13:56:57 +0000

    single article ticket done

--- src/api.js
+++ src/api.js
@@ -5,10 +5,18 @@ const baseUrl = 'https://nc-news-api-qdyy.onrender.com/api'
 export const getAllArticles = async () => {
     try {
         const response = await axios.get(`${baseUrl}/articles`);
-        console.log('API Response:', response.data);
         return response.data.articles;
     } catch (error) {
         throw new Error('Failed to fetch articles');
+    }
+};
+
+export const getArticleById = async (article_id) => {
+    try {
+        const response = await axios.get(`${baseUrl}/articles/${article_id}`);
+        return response.data.article;
+    } catch (error) {
+        throw new Error('Failed to fetch article by ID');
     }
 };
 
